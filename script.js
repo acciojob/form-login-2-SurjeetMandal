0b0a76b0617811ec6689f3fb962770fbe4854e40
SurjeetMandal <surjeetmandal698@gmail.com>
2024-04-07 18:42:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+var btn = document.getElementById('btn');
+
+btn.addEventListener("click",function(event) {
+	event.preventDefault();
+
+	var a = document.getElementById('First_name').value;
+	var b = document.getElementById('Last_name').value;
+	var c = document.getElementById('Phone_number').value;
+	var d = document.getElementById('Email_ID').value;
+	
+	alert("First Name: " + a + "Last Name: " + b + "Phone Number: " + c + "Email ID: " + d);
+})
+
